743f828a9857108af6571bdcceb51fcb35b14520
Patrick J <patryk.jastrzebski91@gmail.com>
2021-11-16 15:35:30 +0100

    Add car model and cars migration

---
+++ database/migrations/2021_11_16_132442_create_cars_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('amount')->default(1);
+            $table->enum('state', ['active', 'inactive']);
+            $table->softDeletes();
+            $table->timestamps();
+
+            /**
+             * @TODO Only for the recruitment task (admin ID).
+             * $table->integer('user_id');
+             * $table->foreign('user_id')->references('id')->on('users');
+             */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
