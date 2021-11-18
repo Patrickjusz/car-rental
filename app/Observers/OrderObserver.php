32d2cf14a84d444a241fdc1eb654b853ec764398
Patrick J <patryk.jastrzebski91@gmail.com>
2021-11-18 18:25:33 +0100

    Delete unused code in OrderObserver

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -21,48 +21,4 @@ class OrderObserver
             dispatch(new SendNewOrderMailJob($order->email));
         }
     }
-
-    /**
-     * Handle the Order "updated" event.
-     *
-     * @param  \App\Models\Order  $order
-     * @return void
-     */
-    public function updated(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Handle the Order "deleted" event.
-     *
-     * @param  \App\Models\Order  $order
-     * @return void
-     */
-    public function deleted(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Handle the Order "restored" event.
-     *
-     * @param  \App\Models\Order  $order
-     * @return void
-     */
-    public function restored(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Handle the Order "force deleted" event.
-     *
-     * @param  \App\Models\Order  $order
-     * @return void
-     */
-    public function forceDeleted(Order $order)
-    {
-        //
-    }
 }
